85b5fe251831f2ab1903135318203c4ba34f0829
Misam Raza <missamraza13@gmail.com>
2025-03-15 18:27:38 +0530

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -138,10 +138,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICROOT = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # ✅ Correct
+]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
